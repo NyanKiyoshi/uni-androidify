64ee55922d3ddebc971e905bcd4c6fc27686f056
NyanKiyoshi <hello@vanille.bid>
2019-01-29 15:10:29 +0100

    Add and implement drawer

---
+++ TP2/AndroidTP2/app/src/main/java/com/example/androidtp2/ButtonActivity.java
@@ -0,0 +1,21 @@
+package com.example.androidtp2;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ButtonActivity extends BaseViewManager {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button catButton = findViewById(R.id.toggle_cat_button);
+        Button loremButton = findViewById(R.id.toggle_lorem_ipsum_button);
+
+        catButton.setOnClickListener(new FragmentToggleListener(
+                this.fragmentManager, this.cuteCatFragment));
+
+        loremButton.setOnClickListener(new FragmentToggleListener(
+                this.fragmentManager, this.loremIpsumFragment));
+    }
+}
---
+++ TP2/AndroidTP2/app/src/main/java/com/example/androidtp2/DrawerActivity.java
@@ -0,0 +1,68 @@
+package com.example.androidtp2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class DrawerActivity extends BaseViewManager
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private void replaceFragment(Fragment newFragment) {
+        FragmentTransaction transaction =
+                this.fragmentManager.beginTransaction();
+        transaction.replace(R.id.content_drawer, newFragment);
+        transaction.commit();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_drawer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_cat) {
+            this.replaceFragment(this.cuteCatFragment);
+        } else if (id == R.id.nav_lorem) {
+            this.replaceFragment(this.loremIpsumFragment);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
