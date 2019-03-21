d66890caf6d05e5ace74112a6dcf01563e9af88b
NyanKiyoshi <hello@vanille.bid>
2019-03-21 07:42:26 +0100

    Fix the contact list picture's pool usage (fixes #40)

--- TP4/AddressBook/app/src/main/java/com/example/addressbook/models/Drawables.java
+++ TP4/AddressBook/app/src/main/java/com/example/addressbook/models/Drawables.java
@@ -8,6 +8,9 @@ public final class Drawables {
     public final static @DrawableRes
     int DefaultContactPic = R.drawable.ic_person_white;
 
+    public final static @DrawableRes
+    int DefaultContactPicDark = R.drawable.ic_icon_contact;
+
     public final static
     int ResolutionThumbnail = 100;
 }
