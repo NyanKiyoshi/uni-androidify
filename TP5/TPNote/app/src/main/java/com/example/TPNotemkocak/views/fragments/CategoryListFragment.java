5984c771ebc39dbcc2252f3c651efa63f0ddf675
NyanKiyoshi <hello@vanille.bid>
2019-03-27 16:00:08 +0100

    Implement category listing and viewing

--- TP5/TPNote/app/src/main/java/com/example/TPNotemkocak/views/fragments/CategoryListFragment.java
+++ TP5/TPNote/app/src/main/java/com/example/TPNotemkocak/views/fragments/CategoryListFragment.java
@@ -1,7 +1,10 @@
 package com.example.TPNotemkocak.views.fragments;
 
+import android.content.Intent;
+
 import com.example.TPNotemkocak.controllers.adapters.CategoryAdapter;
 import com.example.TPNotemkocak.models.CategoryModel;
+import com.example.TPNotemkocak.views.activities.ViewCategoryActivity;
 import com.example.TPNotemkocak.views.viewholders.CategoryViewHolder;
 
 public class CategoryListFragment extends BaseRecyclerFragment<CategoryModel, CategoryViewHolder> {
@@ -9,9 +12,12 @@ public class CategoryListFragment extends BaseRecyclerFragment<CategoryModel, Ca
     public CategoryListFragment() {
         // Create the view adapter
         this.adapter = new CategoryAdapter(this::onSelectItem);
+        this.adapter.addItems(CategoryModel.getAvailableCategories());
     }
 
     public void onSelectItem(CategoryModel item, int pos) {
-
+        Intent intent = new Intent(this.context, ViewCategoryActivity.class);
+        intent.putExtra(ViewCategoryActivity.EXTRA_ID, item.getId());
+        this.startActivityForResult(intent, 1);
     }
 }
